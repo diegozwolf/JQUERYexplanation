085961845d6e2483809f1a98f458cb865bfe4cc7
diegozwolf <diegozuluaga88@gmail.com>
2019-06-19 16:22:35 -0500

    add methods

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// $('div').on('click', function() { //enlazar un evento del navegador ("cLick") al div 
+//     $('div').css('background', 'yellow'); //cambiara el color del elemento
+// });
+
+
 $('div').on('click', function() { //enlazar un evento del navegador ("cLick") al div 
-    $('div').css('background', 'yellow'); //cambiara el color del elemento
+    $(this).css('background', 'yellow'); //this le dara la opcion de que solo el cudro que sea clickeado cambie de color
 });
--- concepts.js
+++ concepts.js
@@ -16,3 +16,56 @@
 $('div').on('click', function() {
     $('div').css('background', 'yellow');
 });
+
+// SELECTORES 
+
+$(p) // Selecciona todos los párrafos del documento.
+$('.item') // Selecciona todos los elementos que tengan la clase item.
+$('#example') // Selecciona todos los elementos que tengan el id example.
+$('.wrapper p') //Selecciona todos los elementos p que estén dentro de otro elemento
+
+// pero que pasa  si se agregan mas div al html ?
+// agregar mas etiquetas div al html
+// revisar que opcion da jquery para que solo un cuadro cambie
+
+
+// INSERTAR ELEMENTOS
+jQuery('body').append("<h1>Hola Mundo</h1>");
+
+// REEMPLAZAR ELEMENTOS
+$('p').replaceWith(`<div>Hola Mundo</div>`);
+
+// ELIMINAR ELEMENTOS
+$('p.test').remove(); // borrara todos los párrafos que tengan la clase test
+
+// AGREAGAR Y MOSTRAR ELEMENTOS
+$('div.step-1').hide(); //esconder 
+$('div.step-2').show(); //mostrar
+$('div.step-2').toggle(); //hace lo contrario si esta visible lo esconde 
+
+
+// Agregando y removiendo clases de CSS
+$('div.step-1').addClass("active");
+$('div.step-2').removeClass("active");
+
+$('div.step-1').toggleClass("active"); // si tiene la clase la remueve, de lo contrario la agrega
+
+
+//Cambiar estilos 
+$('div.step-1').css("background", "red");
+
+//Cambiar multiples estilos 
+$('div.step-1').css("background", "red").css("font-size", "1.3rem");
+
+// Encadenando métodos 
+$('div').addClass("active")
+        .removeClass("pending")
+        .append("<p>Hola Mundo</p>");
+
+
+
+
+
+
+
+
